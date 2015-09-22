763c56a06471f6d9ef6e3a0f1f6c7dd845d5ba32
Tom Scott <tscott@weblinc.com>
2015-09-21 22:45:16 -0400

    Move location of codeclimate test reporter

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,9 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
-require 'active_model/jobs'
-require './spec/support/mocks'
-
 if ENV['CI']
   require "codeclimate-test-reporter"
   CodeClimate::TestReporter.start
 end
+
+require 'active_model/jobs'
+require './spec/support/mocks'
