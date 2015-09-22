18a0d657e3d3d1664cbf75334ea7733c945e3e3f
Tom Scott <tscott@weblinc.com>
2015-09-21 22:37:34 -0400

    Report test coverage to codeclimate

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,8 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'active_model/jobs'
 require './spec/support/mocks'
+
+if ENV['CI']
+  require "codeclimate-test-reporter"
+  CodeClimate::TestReporter.start
+end
