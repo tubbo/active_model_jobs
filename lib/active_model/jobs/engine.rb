dc29c53728a7edb97a1a5c95927d4300006e95b2
Tom Scott <tscott@weblinc.com>
2016-05-11 13:47:00 -0400

    Might be unnecessary

--- lib/active_model/jobs/engine.rb
+++ lib/active_model/jobs/engine.rb
@@ -1,15 +1,7 @@
 if defined? Rails
   module ActiveModel
     module Jobs
-      # Hooks into the host Rails application that include
-      # +ActiveModel::Jobs+ into every model that includes
-      # +ActiveModel::Model+.
       class Engine < Rails::Engine
-        initializer 'active_model_jobs.setup' do
-          ActiveSupport.on_load :active_model do
-            ActiveModel::Model.send :include, ActiveModel::Jobs
-          end
-        end
       end
     end
   end
